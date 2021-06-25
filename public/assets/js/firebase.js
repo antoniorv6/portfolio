e2145918aa9ffa5902ba296a7643b225d91d7aed
antoniorv6 <ariosvila@gmail.com>
2021-06-25 13:41:43 +0200

    Publication information included as popup

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -5,6 +5,10 @@ const publicationsList = document.getElementById("publications_container");
 
 let publicationsRef;
 let unsubscribe;
+let modalContentElement;
+
+modalContentElement = document.getElementById("modalContent")
+
 publicationsRef = db.collection('publications');
 unsubscribe = publicationsRef.orderBy('date', 'desc').onSnapshot(querySnapshot => {
 const items = querySnapshot.docs.map(doc => {
@@ -18,12 +22,50 @@ publicationsList.innerHTML = items.join('');
 });
 
 
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 function getPublication(title)
 {
-    console.log("ug")
     let unsubscribe_pub = publicationsRef.where('title', '==', title).onSnapshot(querySnapshot => {
-        const items = querySnapshot.docs.map(doc => {
-            console.log(doc.data())
-    })})
+        const docitems = querySnapshot.docs.map(doc => {
+            let abstract = doc.data().abstract
+            if (abstract == undefined)
+            {
+              abstract = "-"
+            }
+            return `<span class="close" onclick="closeModal()">&times;</span>
+                    <h2>${doc.data().title}</h2>
+                    <br>
+                    <p><b>Authors</b>: ${doc.data().authors}</p>
+                    <p><b>Published in</b>: ${doc.data().journal}</p>
+                    <p><b>Publishing Date</b>: ${Date(doc.data().date)}</p>
+                    <p><b>Abstract</b>: ${abstract}</p>
+                    <br>
+                    <a href="${doc.data().url}" class="button">Go to the full paper</a>
+                    `
+    })
+    modalContentElement.innerHTML = docitems.join('');
+    modal.style.display = "block";
+  });
 }
 
